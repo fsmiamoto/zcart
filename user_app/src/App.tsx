2bbb36fd622940cdfca4b44ad51fb637fdeca6b3
Francisco Miamoto <francisco@dorayaki.co>
2022-07-13 09:29:38 -0300

    feat(user_app): support product quantities

--- user_app/src/App.tsx
+++ user_app/src/App.tsx
@@ -1,37 +1,43 @@
-import { Layout, List, Avatar, Typography, Button, Statistic } from 'antd';
+import { Layout, List, Avatar, Button, Statistic } from 'antd';
 import "./App.css"
 import React, { useState, useCallback, useEffect } from 'react';
 
 type CartItem = {
     title: string
     price: number
-    description? : string
+    quantity: number
+    description?: string
+    image_url?: string
 }
 
-const defaultCart:  CartItem[] = [
+const PLACEHOLDER_PRODUCT_URL = "./images/placeholder.png"
+
+const defaultCart: CartItem[] = [
     {
         title: 'Coca-Cola 2L',
         price: 6.00,
+        quantity: 1,
     },
     {
         title: 'Batata Ruffles',
         price: 7.00,
+        quantity: 2,
     },
     {
         title: 'Pringles',
-        price: 10.00
+        price: 10.00,
+        quantity: 3,
     },
 ];
 
-const { Title } = Typography;
 const { Header, Content, Footer } = Layout;
 
 function App() {
     const [cartProducts, setCartProducts] = useState(defaultCart);
-    const [subtotal, setSubtotal] = useState(0.0);
+    const [subtotal, setSubtotal] = useState(1.0);
 
     useEffect(() => {
-        setSubtotal(cartProducts.reduce((total, item) => total + item.price, 0.0))
+        setSubtotal(cartProducts.reduce((total, item) => total + item.price * item.quantity, 0.0))
     }, [cartProducts])
 
     const handleEmptyCart = useCallback(
@@ -42,7 +48,7 @@ function App() {
     return (
         <Layout style={{ minHeight: '100vh' }}>
             <Header>
-                <Title level={3} style={{ color: 'white' }}>zCart</Title>
+                <h3 style={{ color: 'white' }}>zCart</h3>
             </Header>
             <Content>
                 <List
@@ -52,11 +58,11 @@ function App() {
                     renderItem={item => (
                         <List.Item>
                             <List.Item.Meta
-                                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                                title={item.title}
+                                avatar={<Avatar src={item.image_url ?? PLACEHOLDER_PRODUCT_URL} />}
+                                title={`${item.quantity}x ${item.title}`}
                                 description={item.description ?? "Produto"}
                             />
-                            <Statistic value={item.price} precision={2} prefix="R$" decimalSeparator="," />
+                            <Statistic value={item.price * item.quantity} precision={2} prefix="R$" decimalSeparator="," />
                         </List.Item>
                     )}
                 />
