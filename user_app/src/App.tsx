dba50faa4d72599c66656c69456ad5ead2699f60
Francisco Miamoto <francisco@dorayaki.co>
2022-07-31 17:16:16 -0300

    feat(user_app): add initial support for cart service

--- user_app/src/App.tsx
+++ user_app/src/App.tsx
@@ -1,82 +1,109 @@
-import { useState, useCallback, useEffect } from 'react';
-import { Layout, List, message, Avatar, Button, Statistic, Modal } from 'antd';
-import { ShoppingCartOutlined } from "@ant-design/icons"
+import { useState, useCallback, useEffect } from "react";
+import { Layout, List, message, Avatar, Button, Statistic, Modal } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
 
-import { CartProvider } from './CartProvider/CartProvider';
+import { CartProvider, CartItem } from "./CartProvider/CartProvider";
 
-import "./App.css"
+import "./App.css";
 
 // FIXME: Remove this, used for testing
-import { StubCartProvider } from './CartProvider/StubCartProvider';
+// import { StubCartProvider } from "./CartProvider/StubCartProvider";
 
 export interface Props {
-    cartProvider: CartProvider
+  cartProvider: CartProvider;
 }
 
 const { Header, Content, Footer } = Layout;
 
 function App(props: Props) {
-    const [cartProducts, setCartProducts] = useState(props.cartProvider.ListCartItems());
-    const [subtotal, setSubtotal] = useState(0.0);
-    const [modalVisible, setModalVisible] = useState(false);
+  const [cartProducts, setCartProducts] = useState<CartItem[]>([]);
+  const [subtotal, setSubtotal] = useState(0.0);
+  const [modalVisible, setModalVisible] = useState(false);
 
-    useEffect(() => {
-        setSubtotal(cartProducts.reduce((total, item) => total + item.price * item.quantity, 0.0))
-    }, [cartProducts])
+  useEffect(() => {
+    props.cartProvider.ListCartItems().then((items) => {
+      setCartProducts(items);
+    });
+  }, [props.cartProvider]);
 
-    const handleFinalize = useCallback(() => {
-        setModalVisible(false)
-        setCartProducts([])
-        message.success("Obrigado!")
-    }, [])
-
-    return (
-        <Layout className="app">
-            <Header className="header">
-                <ShoppingCartOutlined id="icon" />
-                <span id="title">zCart</span>
-            </Header>
-            <Content className="content">
-                <List
-                    className="products"
-                    itemLayout="horizontal"
-                    dataSource={cartProducts}
-                    locale={{ emptyText: "Carrinho vazio" }}
-                    renderItem={item => (
-                        <List.Item className="cart-item">
-                            <List.Item.Meta
-                                className="cart-item-meta"
-                                avatar={<Avatar src={item.image_url} />}
-                                title={`${item.quantity}x ${item.title}`}
-                                description={`R$ ${item.price.toFixed(2).replace(".", ",")}`}
-                            />
-                            <Statistic className="cart-item-price" value={item.price * item.quantity} precision={2} prefix="R$" decimalSeparator="," />
-                        </List.Item>
-                    )}
-                />
-                <div className="subtotal">
-                    <Button type="primary" size="large" onClick={() => setModalVisible(true)} disabled={cartProducts.length === 0} >
-                        Finalizar compra
-                    </Button>
-                    <span>Subtotal: <Statistic value={subtotal} prefix="R$" precision={2} decimalSeparator="," /></span>
-                </div>
-            </Content>
-            <Footer>
-                <div className="buttons">
-                    {/* TODO: Debug this, not triggering re-render */}
-                    <Button onClick={() => { (props.cartProvider as StubCartProvider).AddItem(); console.log(props.cartProvider.ListCartItems()); setCartProducts(props.cartProvider.ListCartItems()) }}>
-                        Click Me to add a item
-                    </Button>
-                    <Button onClick={() => { (props.cartProvider as StubCartProvider).RemoveItem(); setCartProducts(props.cartProvider.ListCartItems()) }}>
-                        Click Me to remove a item
-                    </Button>
-                </div>
-            </Footer>
-            <Modal visible={modalVisible} onOk={handleFinalize} onCancel={() => setModalVisible(false)} okText={"Finalizar"} cancelText={"Cancelar"}>
-                Deseja finalizar a compra?
-            </Modal>
-        </Layout>
+  useEffect(() => {
+    setSubtotal(
+      cartProducts.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0.0
+      )
     );
-};
+  }, [cartProducts]);
 
-export default App; 
+  const handleFinalize = useCallback(() => {
+    setModalVisible(false);
+    setCartProducts([]);
+    message.success("Obrigado!");
+  }, []);
+
+  return (
+    <Layout className="app">
+      <Header className="header">
+        <ShoppingCartOutlined id="icon" />
+        <span id="title">zCart</span>
+      </Header>
+      <Content className="content">
+        <List
+          className="products"
+          itemLayout="horizontal"
+          dataSource={cartProducts}
+          locale={{ emptyText: "Carrinho vazio" }}
+          renderItem={(item) => (
+            <List.Item className="cart-item">
+              <List.Item.Meta
+                className="cart-item-meta"
+                avatar={<Avatar src={item.image_url} />}
+                title={`${item.quantity}x ${item.title}`}
+                description={`R$ ${item.price.toFixed(2).replace(".", ",")}`}
+              />
+              <Statistic
+                className="cart-item-price"
+                value={item.price * item.quantity}
+                precision={2}
+                prefix="R$"
+                decimalSeparator=","
+              />
+            </List.Item>
+          )}
+        />
+        <div className="subtotal">
+          <Button
+            type="primary"
+            size="large"
+            onClick={() => setModalVisible(true)}
+            disabled={cartProducts.length === 0}
+          >
+            Finalizar compra
+          </Button>
+          <span>
+            Subtotal:{" "}
+            <Statistic
+              value={subtotal}
+              prefix="R$"
+              precision={2}
+              decimalSeparator=","
+            />
+          </span>
+        </div>
+      </Content>
+      <Footer>
+      </Footer>
+      <Modal
+        visible={modalVisible}
+        onOk={handleFinalize}
+        onCancel={() => setModalVisible(false)}
+        okText={"Finalizar"}
+        cancelText={"Cancelar"}
+      >
+        Deseja finalizar a compra?
+      </Modal>
+    </Layout>
+  );
+}
+
+export default App;
--- user_app/src/CartProvider/CartProvider.ts
+++ user_app/src/CartProvider/CartProvider.ts
@@ -1,19 +1,18 @@
-
 export interface Item {
-  title: string
-  price: number
-  description?: string
-  image_url?: string
+  title: string;
+  price: number;
+  description?: string;
+  image_url?: string;
 }
 
-export interface CartItem extends Item{
-    quantity: number
+export interface CartItem extends Item {
+  quantity: number;
 }
 
 export interface CartProvider {
-  ListCartItems() : CartItem[]
-  OnAddProduct(handler: ItemHandler): void
-  OnRemoveProduct(handler: ItemHandler): void
+  ListCartItems(): Promise<CartItem[]>;
+  OnAddProduct(handler: ItemHandler): void;
+  OnRemoveProduct(handler: ItemHandler): void;
 }
 
-export type ItemHandler = (item: Item) => void
+export type ItemHandler = (item: Item) => void;
