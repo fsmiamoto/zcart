f9b9fe55987d6a1c47e4168e4046c462141aa949
Francisco Miamoto <francisco@dorayaki.co>
2022-08-14 20:18:13 -0300

    feat(user_app): use loading component

--- user_app/src/CartProvider/CartServiceCartProvider.ts
+++ user_app/src/CartProvider/CartServiceCartProvider.ts
@@ -19,23 +19,63 @@ interface CartServiceCartProduct {
   };
 }
 
-export class CartServiceCartProvider implements CartProvider {
-  private axios: AxiosInstance;
+enum actionType {
+  ADD = 0,
+  REMOVE = 1,
+}
 
-  constructor(url: string) {
+interface cartServiceAction {
+  type: number;
+  cart_product: CartServiceCartProduct;
+}
+
+export class CartServiceCartProvider implements CartProvider {
+  private readonly axios: AxiosInstance;
+  private readonly cartId: string;
+  private readonly websocket: WebSocket;
+  private addProductHandler?: ItemHandler;
+  private removeProductHandler?: ItemHandler;
+
+  constructor(url: string, cartId: string = "2") {
+    this.cartId = cartId;
     this.axios = axios.create({
       baseURL: url,
     });
+    const webSocketUrl = `${url}/cart/${cartId}/ws`.replace("http", "ws");
+    console.log(webSocketUrl);
+    this.websocket = new WebSocket(webSocketUrl);
+    this.websocket.onopen = (_event) => {
+      console.log("opening websocket");
+    };
+    this.websocket.onclose = (_event) => {
+      console.log("closing websocket");
+    };
+    this.websocket.onmessage = (event) => {
+      const payload = JSON.parse(event.data) as cartServiceAction;
+      console.log(payload);
+      if (payload.type === actionType.ADD) {
+        this.addProductHandler &&
+          this.addProductHandler(this.adapter(payload.cart_product));
+      } else {
+        this.removeProductHandler &&
+          this.removeProductHandler(this.adapter(payload.cart_product));
+      }
+    };
   }
 
   async ListCartItems(): Promise<CartItem[]> {
-    const items = (await this.axios.get("/cart/2")).data as CartServiceResponse;
+    const items = (await this.axios.get(`/cart/${this.cartId}`))
+      .data as CartServiceResponse;
     return items.products.map(this.adapter);
   }
 
-  OnAddProduct(handler: ItemHandler) {}
+  OnAddProduct(handler: ItemHandler) {
+    this.addProductHandler = handler;
+  }
 
-  OnRemoveProduct(handler: ItemHandler) {}
+  OnRemoveProduct(handler: ItemHandler) {
+    this.removeProductHandler = handler;
+  }
 
   private adapter(cartProduct: CartServiceCartProduct): CartItem {
     return {
