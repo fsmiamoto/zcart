0385f065ee2e6a545e1caa924f2802a0491a7dd3
F. Miamoto <flavio@dorayaki.co>
2022-08-28 00:02:46 -0300

    feat(product_recognizer): bootstrap python app (#30)

---
+++ product_recognizer/logger.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+
+class Logger(logging.Logger):
+    def __init__(self, name="logger"):
+        super().__init__(name)
+        self.__handler = logging.StreamHandler(sys.stdout)
+        self.__handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        self.addHandler(self.__handler)
+        self.setLevel(logging.DEBUG)
