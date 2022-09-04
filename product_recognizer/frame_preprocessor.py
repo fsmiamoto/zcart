780696aab73e44adea3b126082522106c1a74c95
Flavio Miamoto <flavio@dorayaki.co>
2022-09-04 02:36:58 -0300

    feat(product_recognizer): add poc using video input

---
+++ product_recognizer/frame_preprocessor.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+class FramePreprocessor:
+    def __init__(self, height: int, width: int, mean: float, std: float, is_floating_model: bool):
+        self.__height = height
+        self.__width = width
+        self.__mean = mean
+        self.__std = std
+        self.__is_floating_model = is_floating_model
+
+        pass
+
+    def process(self, frame):
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (self.__width, self.__height))
+
+        result  = np.expand_dims(frame_resized, axis=0)
+
+        if self.__is_floating_model:
+            result = (np.float32(result) - self.__mean) / self.__std
+
+        return result
+
---
+++ product_recognizer/video_stream.py
@@ -0,0 +1,37 @@
+import cv2
+from threading import Thread
+
+# TODO: Type methods
+class VideoStream:
+    def __init__(self,resolution=(640,480)):
+        self.__stream = cv2.VideoCapture(0)
+
+        if not self.__stream.isOpened():
+            raise Exception("failed to open video camera")
+
+        self.__stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        self.__stream.set(3, resolution[0])
+        self.__stream.set(4, resolution[1])
+
+        (self.__grabbed, self.__frame) = self.__stream.read()
+
+        self.__stopped = False
+
+    def start(self):
+        Thread(target=self.__frame_reader,args=()).start()
+        return self
+
+    def stop(self):
+        self.__stopped = True
+
+    def read_frame(self):
+        return self.__frame
+
+    def __frame_reader(self):
+        while True:
+            if self.__stopped:
+                self.__stream.release()
+                return
+
+            (self.__grabbed, self.__frame) = self.__stream.read()
+
