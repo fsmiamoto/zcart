9df8bed2ba54b000518b0af847fdca9e28c43bd5
Flavio Miamoto <flavio@dorayaki.co>
2022-09-04 19:58:24 -0300

    feat(product_recognizer): use weight sensor with video stream to detect changes

--- product_recognizer/main.py
+++ product_recognizer/main.py
@@ -2,7 +2,9 @@
 
 import sys
 import time
+from typing import Dict
 import cv2
+from collections import defaultdict
 
 from frame_preprocessor import FramePreprocessor
 from object_detector import ObjectDetector
@@ -12,7 +14,7 @@ from logger import Logger
 from video_stream import VideoStream
 
 WEIGHT_THRESHOLD = 30
-CONFIDENCE_THRESHOLD = 0.6
+CONFIDENCE_THRESHOLD = 0.55
 
 MODEL_FILE = './model/detect.tflite'
 LABELMAP_FILE = './model/labelmap.txt'
@@ -26,10 +28,6 @@ FRAME_HEIGHT = 480
 
 CV2_WINDOW_NAME='Object Detection'
 
-# TODOs:
-# - Move CV2 Logic to another module
-# - Create logic for diff'ing objects
-
 def main():
     log = Logger()
     weight_sensor = WeightSensor()
@@ -38,7 +36,7 @@ def main():
     height, width = detector.get_input_dimensions()
 
     preprocessor = FramePreprocessor(height,width, INPUT_MEAN, INPUT_STD, detector.is_floating_model())
-    # cart_service_client = CartServiceClient("http://tokyo:3333")
+    cart_service_client = CartServiceClient("http://tokyo:3333")
 
     log.info("will start video stream")
     stream = VideoStream(resolution=(FRAME_WIDTH, FRAME_HEIGHT)).start()
@@ -54,6 +52,31 @@ def main():
     frame_rate = 0
     tick_frequency = cv2.getTickFrequency()
 
+    last_weight_reading = 0.0
+    last_frame_objects = {}
+
+    # TODO: Move to module
+    def build_object_dict(objects) -> Dict[str,int]:
+        result = defaultdict(int)
+        for (_, _, class_index) in objects:
+            result[detector.get_label(int(class_index))] += 1
+        return result
+
+    def get_difference(current_frame_objects: Dict[str,int], last_frame_objects: Dict[str,int]) -> Dict[str, int]:
+        result = {}
+        for label in current_frame_objects:
+            result[label] = current_frame_objects[label]
+        for label in last_frame_objects:
+            if label in result:
+                # Might be negative
+                result[label] -= last_frame_objects[label]
+                if result[label] == 0:
+                    del result[label]
+            else:
+                result[label] = -last_frame_objects[label]
+
+        return result
+
     while True:
         try:
             start_tick = cv2.getTickCount()
@@ -64,31 +87,56 @@ def main():
 
             boxes, classes, scores = detector.get_boxes(), detector.get_classes(), detector.get_scores()
 
-            filtered_objects = filter(lambda tuple: tuple[0] >= CONFIDENCE_THRESHOLD, zip(scores,boxes,classes))
+            filtered_objects = list(
+                filter(lambda tuple: tuple[0] >= CONFIDENCE_THRESHOLD, zip(scores,boxes,classes))
+            )
 
-            for i, (score, box, class_index) in enumerate(filtered_objects):
-                ymin = int(max(1,(box[0] * FRAME_HEIGHT)))
-                xmin = int(max(1,(box[1] * FRAME_WIDTH)))
-                ymax = int(min(FRAME_HEIGHT,(box[2] * FRAME_HEIGHT)))
-                xmax = int(min(FRAME_WIDTH,(box[3] * FRAME_WIDTH)))
+            current_frame_objects = build_object_dict(filtered_objects)
 
-                cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+            diff = get_difference(current_frame_objects, last_frame_objects)
 
-                object_name = detector.get_label(int(class_index))
+            # TODO: This should be a thread
+            for label, count in diff.items():
+                if label != 'bottle':
+                    continue
 
-                # Example: bottle: 75%
-                label = f'{object_name}: {int(score*100)}%'
+                current_weight_reading = weight_sensor.get_reading()
+                if abs(current_weight_reading-last_weight_reading) < WEIGHT_THRESHOLD:
+                    log.info("ignoring, not reached weight threshold")
+                    continue
 
-                labelSize, _baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
-                label_ymin = max(ymin, labelSize[1],  10) # Make sure not to draw label too close to top of window
-                cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+                log.info("will call cart service")
+                request = UpdateCartRequest("1", abs(count), UpdateCartRequestAction.ADD if count > 0 else UpdateCartRequestAction.REMOVE)
+                response = cart_service_client.execute("2", request)
+                log.info(f"got status {response.status_code}")
+                last_weight_reading = current_weight_reading
 
-                # Draw circle in center
-                xcenter = xmin + (int(round((xmax - xmin) / 2)))
-                ycenter = ymin + (int(round((ymax - ymin) / 2)))
-                cv2.circle(frame, (xcenter, ycenter), 5, (0,0,255), thickness=-1)
+            log.info(f"diff: {diff}")
 
-                log.info(f"Object {i}: {object_name} at ({xcenter},{ycenter}) with confidence {scores[i]*100}")
+            # Display rectangle
+            # for i, (score, box, class_index) in enumerate(filtered_objects):
+            #     ymin = int(max(1,(box[0] * FRAME_HEIGHT)))
+            #     xmin = int(max(1,(box[1] * FRAME_WIDTH)))
+            #     ymax = int(min(FRAME_HEIGHT,(box[2] * FRAME_HEIGHT)))
+            #     xmax = int(min(FRAME_WIDTH,(box[3] * FRAME_WIDTH)))
+            #
+            #     cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+            #
+            #     object_name = detector.get_label(int(class_index))
+            #
+            #     # Example: bottle: 75%
+            #     label = f'{object_name}: {int(score*100)}%'
+            #
+            #     labelSize, _baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+            #     label_ymin = max(ymin, labelSize[1],  10) # Make sure not to draw label too close to top of window
+            #     cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+            #
+            #     # Draw circle in center
+            #     xcenter = xmin + (int(round((xmax - xmin) / 2)))
+            #     ycenter = ymin + (int(round((ymax - ymin) / 2)))
+            #     cv2.circle(frame, (xcenter, ycenter), 5, (0,0,255), thickness=-1)
+            #
+            #     log.info(f"Object {i}: {object_name} at ({xcenter},{ycenter}) with confidence {scores[i]*100}")
 
             cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
             cv2.imshow(CV2_WINDOW_NAME, frame)
@@ -96,8 +144,11 @@ def main():
             end_tick = cv2.getTickCount()
             frame_rate = tick_frequency/(end_tick-start_tick)
 
+            last_frame_objects = current_frame_objects
+
             # Need to call waitKey to display the frame
             cv2.waitKey(1)
+
 
         except (KeyboardInterrupt, SystemExit):
             log.info("received exit signal, cleaning up")
@@ -105,6 +156,7 @@ def main():
             cv2.destroyAllWindows()
             stream.stop()
             sys.exit()
+
 
 if __name__ == '__main__':
     main()
